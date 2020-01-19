8c23de955db30a64cc1e3d6306fe489854ef5923
Attilio Furci <lxmediacom@gmail.com>
2020-01-19 14:05:07 +0100

    Register / Login with Bcrypt

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -9,5 +9,8 @@ module.exports = {
 			host: process.env.HOST || 'remotemysql.com',
 			storage: './lxcodebase.sqlite'
 		}
+	},
+	authentication: {
+		jwtSecret: process.env.JWT_SECRET || 'secret'
 	}
 }
