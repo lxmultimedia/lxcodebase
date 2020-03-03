c5ed057d43cb01ce839395f0bef33e10501523c9
Attilio Furci <lxmediacom@gmail.com>
2020-03-03 19:44:40 +0100

    material support

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,24 +1,33 @@
-s<template>
-  <div>
-    <h1>Register</h1>
-    <input
-      type="email"
-      name="email"
-      v-model = "email"
-      placeholder="email" />
-    <br>
-    <input
-      type="password"
-      name="password"
-      v-model = "password"
-      placeholder="password" />
-    <br>
-    <div class="error" v-html="error"></div>
-    <button
-      @click="register">
-      Register
-    </button>
-  </div>
+<template>
+  <v-layout column>
+    <v-container>
+      <div class="white elevation-2">
+        <v-toolbar flat dense class="cyan" dark>
+          <v-toolbar-title>Register</v-toolbar-title>
+        </v-toolbar>
+        <div class="pl-4 pr-4 pt-2 pb-2">
+          <v-text-field
+            type="email"
+            name="email"
+            v-model = "email"
+            placeholder="email" />
+          <br>
+          <v-text-field
+            type="password"
+            name="password"
+            v-model = "password"
+            placeholder="password" />
+          <br>
+          <div class="error" v-html="error"></div>
+          <v-btn
+            class="cyan"
+            @click="register">
+            Register
+          </v-btn>        
+          </div>
+      </div>
+    </v-container>
+  </v-layout>
 </template>
 
 <script>
