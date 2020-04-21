5cf218adb517e12f45490a26d5fa241f1498a154
Attilio Furci <lxmediacom@gmail.com>
2020-04-21 12:54:27 +0200

    styling

--- client/src/components/Login.vue
+++ client/src/components/Login.vue
@@ -2,33 +2,39 @@
   <v-layout column>
     <v-container>
       <v-flex xs8 offset-xs2>
-        <v-toolbar dark
-        color="indigo">
-          Login
-        </v-toolbar>
-        <div class="white elevation-2">
-          <div class="pl-4 pr-4 pt-2 pb-2">
-            <v-text-field
-              type="email"
-              name="email"
-              v-model = "email"
-              placeholder="email" />
-            <br>
-            <v-text-field
-              type="password"
-              name="password"
-              v-model = "password"
-              placeholder="password" />
-            <br>
-            <div class="error" v-html="error"></div>
-            <v-btn
-              dark
-              large
-              class="indigo"
-              @click="login">
-              Login
-            </v-btn>
-            </div>
+        <div class="white">
+          <v-toolbar dark
+          color="indigo">
+            Login
+          </v-toolbar>
+          <div class="pl-4 pr-4 pt-2 pb-2 panel">
+            <form
+            name="lxcodebase_login">
+              <v-text-field
+                type="email"
+                name="email"
+                v-model = "email"
+                placeholder="email"
+                @keyup.enter.native = "onSubmit()" />
+              <br>
+              <v-text-field
+                type="password"
+                name="password"
+                v-model = "password"
+                placeholder = "password"
+                @keyup.enter.native = "onSubmit()" />
+              <div class="error" v-html="error" v-if="error"></div>
+              <br>
+              <v-btn
+                id="btnSubmit"
+                dark
+                large
+                class="indigo"
+                @click="login">
+                Login
+              </v-btn>
+            </form>
+          </div>
         </div>
       </v-flex>
     </v-container>
@@ -58,6 +64,9 @@ export default {
       } catch (error) {
         this.error = error.response.data.error
       }
+    },
+    onSubmit () {
+      this.login()
     }
   }
 }
@@ -65,8 +74,15 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  .error {
-    color: white;
-    margin: 15px 0px;
-  }
+.error {
+  color: white;
+  margin: 15px 0px;
+  padding: 5px;
+}
+#btnSubmit {
+  margin-bottom: 20px;
+}
+.panel {
+  border: 1px solid #ddd;
+}
 </style>
--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -2,12 +2,12 @@
   <v-layout>
     <v-container>
       <v-flex xs8 offset-xs2>
-        <v-toolbar dark
-        color="indigo">
-          Register
-        </v-toolbar>
-        <div class="white elevation-2">
-          <div class="pl-4 pr-4 pt-2 pb-2">
+        <div class="white">
+          <v-toolbar dark
+          color="indigo">
+            Register
+          </v-toolbar>
+          <div class="pl-4 pr-4 pt-2 pb-2 panel">
             <form
             name="lxcodebase_register"
             autocomplete="off">
@@ -15,25 +15,28 @@
                 type="email"
                 name="email"
                 v-model = "email"
-                placeholder="email" />
+                placeholder="email"
+                @keyup.enter.native = "onSubmit()" />
               <br>
               <v-text-field
                 type="password"
                 name="password"
                 v-model = "password"
                 placeholder="password"
-                autocomplete="new-password"/>
+                autocomplete="new-password"
+                @keyup.enter.native = "onSubmit()" />
+              <div class="error" v-html="error" v-if="error"></div>
               <br>
-              <div class="error" v-html="error"></div>
               <v-btn
+                id="btnSubmit"
                 dark
                 large
                 class="indigo"
                 @click="register">
                 Register
               </v-btn>
-              </form>
-            </div>
+            </form>
+          </div>
         </div>
       </v-flex>
     </v-container>
@@ -59,9 +62,13 @@ export default {
         })
         this.$store.dispatch('setToken', response.data.token)
         this.$store.dispatch('setUser', response.data.user)
+        this.$router.push('/browse')
       } catch (error) {
         this.error = error.response.data.error
       }
+    },
+    onSubmit () {
+      this.register()
     }
   }
 }
@@ -69,7 +76,15 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  .error {
-    color: red;
-  }
+.error {
+  color: white;
+  margin: 15px 0px;
+  padding: 5px;
+}
+.panel {
+  border: 1px solid #ddd;
+}
+#btnSubmit {
+  margin-bottom: 20px;
+}
 </style>
