e0e11e79152c83fcd706890ea24c57ec83fcf63c
Attilio Furci <lxmediacom@gmail.com>
2021-05-29 23:04:48 +0200

    update for google cloud server

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -3,21 +3,24 @@ import store from '@/store/store'
 
 export default () => {
   const instance = axios.create({
-    baseURL: 'https://lxcodeserver.herokuapp.com/',
+    baseURL: 'https://lxcodebaseserver.oa.r.appspot.com',
     // baseURL: 'http://localhost:8081/',
-    headers: {'Authorization': 'Bearer ' + store.getters.getToken}
+    headers: { Authorization: 'Bearer ' + store.getters.getToken }
   })
 
-  instance.interceptors.response.use((response) => {
-    tokenCheck()
-    return response
-  }, (error) => {
-    if (error.response.status === 403) {
+  instance.interceptors.response.use(
+    response => {
       tokenCheck()
+      return response
+    },
+    error => {
+      if (error.response.status === 403) {
+        tokenCheck()
+        return Promise.reject(error.message)
+      }
       return Promise.reject(error.message)
     }
-    return Promise.reject(error.message)
-  })
+  )
 
   function tokenCheck () {
     const tokenExp = store.getters.getTokenExpires
